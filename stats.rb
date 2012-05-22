8a2a71bc88602a0648665f4e997f10da677e43d7
Adam Gibbins <adam.gibbins@timgroup.com>
2012-05-22 15:31:33 +0100

    Require rubygems to ensure its loaded prior to searching for eventmachine etc

--- stats.rb
+++ stats.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'eventmachine'
 require 'statsd'
 require 'statsd/server'
@@ -14,7 +15,7 @@ db = Mongo::Connection.new(APP_CONFIG['mongo_host']).db(APP_CONFIG['mongo_databa
 APP_CONFIG['retentions'].each do |retention|
   collection_name = retention['name']
   unless db.collection_names.include?(collection_name)
-    db.create_collection(collection_name, :capped => retention['capped'], :size => retention['cap_bytes']) 
+    db.create_collection(collection_name, :capped => retention['capped'], :size => retention['cap_bytes'])
   end
   db.collection(collection_name).ensure_index([['ts', Mongo::ASCENDING]])
 end
@@ -25,27 +26,27 @@ Statsd::Mongo.database = APP_CONFIG['mongo_database']
 Statsd::Mongo.retentions = APP_CONFIG['retentions']
 Statsd::Mongo.flush_interval = APP_CONFIG['flush_interval']
 EventMachine::run do
-  EventMachine::open_datagram_socket('127.0.0.1', 8125, Statsd::Server)  
+  EventMachine::open_datagram_socket('127.0.0.1', 8125, Statsd::Server)
   EventMachine::add_periodic_timer(APP_CONFIG['flush_interval']) do
      counters,timers = Statsd::Server.get_and_clear_stats!
-     
+
      #
      # Flush Adapters
      #
      # Mongo
-     # EM.defer do 
+     # EM.defer do
      #   Statsd::Mongo.flush_stats(counters,timers)
      # end
      #
-     
+
      # Graphite
      EventMachine.connect APP_CONFIG['graphite_host'], APP_CONFIG['graphite_port'], Statsd::Graphite do |conn|
        conn.counters = counters
        conn.timers = timers
        conn.flush_interval = 10
        conn.flush_stats
-     end     
+     end
   end
-  
-  
+
+
 end
